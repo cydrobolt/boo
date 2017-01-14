7db67d2cc786c70aa99fc512d7224d03449c5163
Chaoyi Zha <summermontreal@gmail.com>
2017-01-13 23:04:26 -0500

    add slow typing

--- boo/__init__.py
+++ boo/__init__.py
@@ -8,6 +8,14 @@ k = PyKeyboard()
 def is_halloween_in_january():
     return time.strftime("%d") == "13" and time.strftime("%m") == "01"
 
+def type_slowly(text):
+    for char in text:
+        k.tap_key(char)
+        if char == ' ':
+            time.sleep(0.6)
+        else:
+            time.sleep(0.4)
+
 def spook():
     if not is_halloween_in_january():
         return
@@ -15,6 +23,7 @@ def spook():
     time.sleep(3)
     k.tap_key('F6')
     time.sleep(1)
-    k.type_string('hackclub.com')
+    # k.type_string('hackclub.com')
+    type_slowly('hackclub.com')
     time.sleep(1)
     k.tap_key('Return')
